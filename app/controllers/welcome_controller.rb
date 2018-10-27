9794f235a6e479264b3fb057ce11c0db04ee5860
Saurabh <dsaurabhd@outlook.com>
2018-10-27 00:43:30 -0700

    Add other tabs

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,4 +4,13 @@ class WelcomeController < ApplicationController
 
   def about
   end
+
+  def services
+  end
+
+  def blog
+  end
+
+  def contact
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,12 @@ Rails.application.routes.draw do
 
   get '/about' => 'welcome#about'
 
+  get '/services' => 'welcome#services'
+
+  get '/blog' => 'welcome#blog'
+
+  get '/contact' => 'welcome#contact'
+
    root 'welcome#homepage'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
